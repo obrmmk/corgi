25b808d172c149c760bb339a9710da3d4220f289
Momoka <79566882+obrmmk@users.noreply.github.com>
2022-01-17 09:45:27 +0900

    Update __init__.py

--- __init__.py
+++ __init__.py
@@ -200,6 +200,6 @@ def run_corgi(nmt, delay=600, print=print_nop):
     display(IPython.display.HTML(HTML))
 
 
-def start_corgi(model_id='1u1RfRFZ5o7CfBhaCaUJLDuwjhI2E8R3G', delay=600, print=print_nop):#211229
+def start_corgi(model_id='1-E4obbu9q7iPUtdPoKFr_gppmYT_o4uF', delay=600, print=print_nop):#211229
     nmt = compose_nmt(generate_nmt(model_id=model_id))
     run_corgi(nmt, delay=delay, print=print)
